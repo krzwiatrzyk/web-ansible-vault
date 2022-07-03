2ae689b515f22c43a3f38b0f9477c4b43d51236f
Krzysztof Wiatrzyk <krzysztof.wiatrzyk@comarch.com>
2022-07-03 21:06:00 +0200

    feat(deploy): add skaffold installing tooljet

--- openfaas-function/web-ansible-vault/run.sh
+++ openfaas-function/web-ansible-vault/run.sh
@@ -6,10 +6,13 @@
 
 touch /tmp/data /tmp/vault_password.txt
 chmod 666 /tmp/data /tmp/vault_password.txt
-cat /dev/stdin > /tmp/data
+cat /dev/stdin > /tmp/data.yaml
 
 #printenv Http_Query
-VAULT_PASSWORD=$(echo ${Http_Query} | cut -f 2 -d "=")
+VAULT_PASSWORD=$(echo ${Http_Vault_password} | cut -f 2 -d "=")
 echo ${VAULT_PASSWORD} > /tmp/vault_password.txt
 
-ansible-vault encrypt /tmp/data --vault-password-file=/tmp/vault_password.txt --output - | tail -n +1
+cat /tmp/data.yaml
+cat /tmp/vault_password.txt
+
+# ansible-vault encrypt /tmp/data.yaml --vault-password-file=/tmp/vault_password.txt --output - | tail -n +1
